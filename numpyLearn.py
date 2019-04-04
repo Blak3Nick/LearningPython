67671aa4ca18d10980cd0ff8ab6f354aa6b65db7
Blak3Nick <blaken2021@gmail.com>
2019-04-04 07:58:52 -0400

    working with pandas

--- numpyLearn.py
+++ numpyLearn.py
@@ -1,5 +1,8 @@
 import numpy as np
 array = [2, 4, 67, 1, 4, 6]
-print (np.argsort(array))
+print(np.argsort(array))
 taxi = np.genfromtxt('nyc_taxis.csv', delimiter=',', skip_header=True)
 print(taxi.dtype)
+
+trip_mph = taxi[:,7] / (taxi[:,8] / 3600)
+taxi_add = np.append(taxi, trip_mph)

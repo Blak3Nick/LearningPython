179eaf76846b49ec3cacb71c3fbaa46151e952a2
Blak3Nick <blaken2021@gmail.com>
2019-04-05 17:04:17 -0400

    working with pandas to clean data

--- clean_data.py
+++ clean_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 laptops = pd.read_csv("laptops.csv", encoding="Latin-1")
 
+
 def clean_c(string):
     string = string.strip()
     string = string.replace("Operating System", "os")
@@ -15,3 +16,22 @@ for c in laptops.columns:
     new_name = clean_c(c)
     new_columns.append(new_name)
 laptops.columns = new_columns
+
+ram = laptops["ram"]
+laptops.rename({"ram": "ram_gb"}, axis=1, inplace=True)
+laptops["ram_gb"] = laptops["ram_gb"].str.replace("GB", "")
+laptops["ram_gb"] = laptops["ram_gb"].astype(int)
+dtypes = laptops.dtypes
+weight = laptops["weight"]
+laptops["weight"] = laptops["weight"].str.replace("kg", "")
+laptops["weight"] = laptops["weight"].str.replace("s", "")
+unique = laptops["weight"].unique()
+laptops["weight"] = laptops["weight"].astype(float)
+laptops.rename({"weight": "weight_kg"}, axis=1, inplace=True)
+laptops["price_euros"] = laptops["price_euros"].str.replace(",", ".")
+laptops["price_euros"] = laptops["price_euros"].astype(float)
+
+weight_describe = laptops["weight_kg"].describe()
+price_describe = laptops["price_euros"].describe()
+laptops["cpu_manufacturer"] = (laptops["cpu"].str.split(n=1, expand=True).iloc[:, 0])
+

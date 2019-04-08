254aff69b61b86277c904813604362cc6c761680
Blak3Nick <blaken2021@gmail.com>
2019-04-08 16:38:52 -0400

    working with pandas to clean data

--- clean_data.py
+++ clean_data.py
@@ -34,4 +34,60 @@ laptops["price_euros"] = laptops["price_euros"].astype(float)
 weight_describe = laptops["weight_kg"].describe()
 price_describe = laptops["price_euros"].describe()
 laptops["cpu_manufacturer"] = (laptops["cpu"].str.split(n=1, expand=True).iloc[:, 0])
+ghz = laptops["cpu"]
+ghz_string = ghz.str.rsplit(n=1, expand=True)
+ghz_string.loc[:,1] = ghz_string.loc[:,1].str.replace("GHz", "")
+print(ghz_string.loc[:,1].unique())
+laptops["cpu_speed_ghz"] = ghz_string.loc[:,1].astype(float)
 
+mapping_dict = {
+    'Android': 'Android',
+    'Chrome OS': 'Chrome OS',
+    'Linux': 'Linux',
+    'Mac OS': 'macOS',
+    'No OS': 'No OS',
+    'Windows': 'Windows',
+    'macOS': 'macOS'
+}
+laptops["os"] = laptops["os"].map(mapping_dict)
+laptops_no_null_rows = laptops.dropna()
+laptops_no_null_cols = laptops.dropna(axis=1)
+laptops.loc[laptops["os"] == "No OS", "os_version"] = "Version Unknown"
+
+version_unknown = laptops.loc[laptops["os_version"] == "No OS"]
+
+print(laptops.loc[laptops["os_version"].isnull(), "os"].value_counts())
+
+
+laptops["storage_1_capacity_gb"] = laptops["storage"].str.split(n=1)
+second_half = laptops["storage_1_capacity_gb"].values
+
+for i in range(len(second_half)):
+    second_half[i] = second_half.item(i)[1]
+laptops.rename({"storage_1_capacity_gb": "storage_1_type"}, axis=1, inplace=True)
+first_half = laptops["storage"].str.split(expand=True)
+
+first_storage = first_half.loc[:,0]
+first_type = first_half.loc[:,1]
+
+laptops["storage_1_type"] = first_type
+laptops["storage_1_capacity_gb"] = first_storage
+
+laptops["storage_1_capacity_gb"] = laptops["storage_1_capacity_gb"].str.replace("GB", "")
+laptops.loc[laptops["storage_1_capacity_gb"] == "1TB", "storage_1_capacity_gb"] = "1000"
+laptops.loc[laptops["storage_1_capacity_gb"] == "2TB", "storage_1_capacity_gb"] = "2000"
+laptops["storage_1_capacity_gb"] = laptops["storage_1_capacity_gb"].astype(float)
+laptops["storage_2_capacity_gb"] = first_half[3]
+first_half[4].unique()
+laptops.loc[laptops["storage_2_capacity_gb"] == "+", "storage_2_capacity_gb" ] = "1TB"
+laptops["storage_2_capacity_gb"] = laptops["storage_2_capacity_gb"].str.replace("GB", "")
+laptops["storage_2_capacity_gb"] = laptops["storage_2_capacity_gb"].str.replace("1TB", "1000")
+laptops["storage_2_capacity_gb"] = laptops["storage_2_capacity_gb"].str.replace("2TB", "2000")
+print(laptops["storage_2_capacity_gb"].unique())
+laptops["storage_2_capacity_gb"] = laptops["storage_2_capacity_gb"].astype(float)
+# first_half.loc[first_half.loc[:,5] == "HDD", first_half.loc[4]] = "HDD"
+
+first_half[4, 447] = "HDD"
+laptops["storage_2_type"] = first_half[4]
+
+laptops["storage_2_type"] = laptops["storage_2_type"].str.replace("1TB", "HDD")
--- math_plot_lib.py
+++ math_plot_lib.py
@@ -18,3 +18,60 @@ plt.xlabel('Month')
 plt.ylabel("Unemployment Rate")
 plt.title("Monthly Unemployment Trends, 1948")
 plt.show()
+
+fig = plt.figure(figsize=(12,5))
+
+ax1 = fig.add_subplot(2,1,1)
+ax2 = fig.add_subplot(2,1,2)
+ax1.plot(unrate[0:12]['DATE'], unrate[0:12]['VALUE'])
+ax1.set_title('Monthly Unemployment Rate, 1948')
+ax2.plot(unrate[12:24]['DATE'], unrate[12:24]['VALUE'])
+ax2.set_title('Monthly Unemployment Rate, 1949')
+plt.show()
+
+fig = plt.figure(figsize=(12,12))
+forty_eight = fig.add_subplot(5,1,1)
+forty_eight.plot(unrate[0:12]['DATE'], unrate[0:12]['VALUE'])
+forty_nine = fig.add_subplot(5,1,2)
+forty_nine.plot(unrate[12:24]['DATE'], unrate[12:24]['VALUE'])
+fifty = fig.add_subplot(5,1,3)
+fifty.plot(unrate[24:36]['DATE'], unrate[24:36]['VALUE'])
+fifty_one = fig.add_subplot(5,1,4)
+fifty_one.plot(unrate[36:48]['DATE'], unrate[36:48]['VALUE'])
+fifty_two = fig.add_subplot(5,1,5)
+fifty_two.plot(unrate[48:60]['DATE'], unrate[48:60]['VALUE'])
+plt.show()
+
+unrate.describe()
+
+unrate['MONTH'] = unrate['DATE'].dt.month
+
+fig = plt.figure(figsize=(10,6))
+
+plt.plot(unrate[0:12]['MONTH'], unrate[0:12]['VALUE'], c='red', label='1948')
+plt.plot(unrate[12:24]['MONTH'], unrate[12:24]['VALUE'], c='blue', label='1949')
+plt.plot(unrate[24:36]['MONTH'], unrate[24:36]['VALUE'], c='green', label='1950')
+plt.plot(unrate[36:48]['MONTH'], unrate[36:48]['VALUE'], c='orange', label='1951')
+plt.plot(unrate[48:60]['MONTH'], unrate[48:60]['VALUE'], c='black', label='1952')
+plt.legend(loc='upper left')
+plt.show()
+
+year = 1947
+year_string = str(year)
+print(year_string)
+
+fig = plt.figure(figsize=(10,6))
+colors = ['red', 'blue', 'green', 'orange', 'black']
+year = 1947
+for i in range(5):
+    year += 1
+    year_string = str(year)
+    start_index = i*12
+    end_index = (i+1)*12
+    subset = unrate[start_index:end_index]
+    plt.plot(subset['MONTH'], subset['VALUE'], c=colors[i], label=year_string)
+plt.legend(loc='upper left')
+plt.title("Monthly Unemployment Trends, 1948-1952")
+plt.xlabel('Month, Integer')
+plt.ylabel('Unemployment Rate, Percent')
+plt.show()
